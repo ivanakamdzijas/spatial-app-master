66e40f105e1098daa83db2c821eb8b4550d57943
ivanakamdzijas <76914605+ivanakamdzijas@users.noreply.github.com>
2022-04-14 18:56:10 +0200

    Delete app directory

--- app/src/main/java/com/spatial/postgis/services/TouristStatService.java
+++
@@ -1,9 +0,0 @@
-package com.spatial.postgis.services;
-
-import java.util.List;
-
-import com.spatial.postgis.entities.TouristStat;
-
-public interface TouristStatService {
-	List<TouristStat> findAll();
-}
